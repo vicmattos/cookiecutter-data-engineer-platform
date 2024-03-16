590715142e39cfe95bf694202b424fcb3348aa53
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2024-03-16 00:06:06 -0300

    refactor(extract): isolate execution logic from cli call

--- {{cookiecutter.project_directory}}/extract/randomuser/main.py
+++ {{cookiecutter.project_directory}}/extract/randomuser/main.py
@@ -1,47 +1,56 @@
-import os
+from dataclasses import dataclass
 from pathlib import Path
 import tempfile
 import time
 
-from dotenv import load_dotenv
-import boto3
 import jsonlines
 from randomuser import RandomUser
 
 
-# Args
-load_dotenv()
-APP_USERS_QTD: int = int(os.getenv("APP_USERS_QTD", 10))
-APP_BUCKET_NAME: str = os.getenv("APP_BUCKET_NAME")
-APP_OBJ_PREFIX: Path = Path(os.getenv("APP_OBJ_PREFIX"))
-ACCESS_KEY: str = os.getenv("ACCESS_KEY")
-SECRET_KEY: str = os.getenv("SECRET_KEY")
-ENDPOINT_URL: str = os.getenv("ENDPOINT_URL")
+@dataclass
+class Args:
+    user_qtd: int
+    bucket_name: str
+    obj_prefix: Path
 
-FILE_NAME: str = f'{int(time.time())}_users.jsonl'
-TEMP_FILE: Path = Path(tempfile.mkdtemp(), FILE_NAME)
-APP_OBJ_KEY: Path = Path(APP_OBJ_PREFIX, FILE_NAME)
+def main(s3_client, args: Args) -> str:
+    # Clean args
+    args.obj_prefix = _clean_obj_prefix(args.obj_prefix)
+    # Create Constants
+    FILE_NAME: str = f'{int(time.time())}_users.jsonl'
+    TEMP_FILE: Path = Path(tempfile.mkdtemp(), FILE_NAME)
+    S3_OBJ_KEY: Path = Path(args.obj_prefix, FILE_NAME)
+    # Create local file with random users
+    users = generate_users(args.user_qtd)
+    create_file(TEMP_FILE, users)
+    # Upload file to object store
+    s3_client.upload_file(
+        Filename=TEMP_FILE,
+        Bucket=args.bucket_name,
+        Key=str(S3_OBJ_KEY),
+    )
+    # Return object S3 URI
+    obj_uri = Path(args.bucket_name, S3_OBJ_KEY)
+    return 's3://' + str(obj_uri)
 
 
-# Generate Data
-with jsonlines.open(TEMP_FILE, mode='w') as writer:
-    for u in RandomUser.generate_users(APP_USERS_QTD):
-        writer.write({
-            "full_name": u.get_full_name(),
-            "date_of_birth": u.get_dob(),
-            "email": u.get_email(),
+def generate_users(qtd: int) -> list[dict[str,str]]:
+    users = []
+    for ru in RandomUser.generate_users(qtd):
+        users.append({
+            "full_name": ru.get_full_name(),
+            "date_of_birth": ru.get_dob(),
+            "email": ru.get_email(),
         })
+    return users
 
 
-# Upload to S3
-client = boto3.client('s3',
-    endpoint_url=ENDPOINT_URL,
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-)
+def _clean_obj_prefix(obj_prefix: Path) -> Path:
+    if '/' in obj_prefix.parts:
+        return Path(*obj_prefix.parts[1:])
+    return obj_prefix
 
-client.upload_file(
-    Filename=TEMP_FILE,
-    Bucket=APP_BUCKET_NAME,
-    Key=str(APP_OBJ_KEY),
-)
+
+def create_file(file_path: Path, users: list[dict[str,str]]) -> None:
+    with jsonlines.open(file_path, mode='w') as writer:
+        writer.write_all(users)
